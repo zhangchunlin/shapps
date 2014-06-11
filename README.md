22aaff4ec97cb8d9b437e9a5d231d7229bd29aea
zhangclb <zhangclb@lenovo.com>
2014-06-11 10:06:17 +0800

    add auth.superuser Change-Id: I76d81a8c9bcb57083fde957588f5884de2e3254d

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 Shapps
 ========================================
 
-> Chunlin Zhang <zhangchunlin@gmail.com> ,Jiannan Tang <tangjn518@gmail.com>
+Authors
+----------------
+Chunlin Zhang <zhangchunlin@gmail.com> ,Jiannan Tang <tangjn518@gmail.com>
 
 About Shapps
 ----------------
@@ -10,6 +12,6 @@ Shapps = SHare APPS.
 Uliweb ( https://github.com/limodou/uliweb ) is a Python based web framework. Shapps is an apps collection project of uliweb like Plugs ( https://github.com/limodou/plugs ).
 
 License
-------------
+----------------
 
 Shapps is released under BSD license.
---
+++ shapps/auth/superuser/commands.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+#coding=utf-8
+
+from uliweb.core.commands import Command
+from uliweb.orm import get_model
+
+class SuperuserListCommand(Command):
+    name = 'suserlist'
+    help = 'Super user list'
+    
+    def handle(self, options, global_options, *args):
+        self.get_application(global_options)
+        
+        User = get_model("user")
+        for user in User.filter(User.c.is_superuser==True):
+            print user.id, user.username, user.email
+
+class SuperuserSetCommand(Command):
+    name = 'suserset'
+    help = 'Super user set,usage "uliweb %s USERNAME"'%(name)
+    
+    def handle(self, options, global_options, *args):
+        self.get_application(global_options)
+        
+        if len(args)>=1:
+            username = args[0]
+        else:
+            print "usage: 'uliweb %s USERNAME'"%(self.name)
+            return
+        
+        User = get_model("user")
+        user = User.get(User.c.username == username)
+        if user:
+            print "%d %s is_superuser=%s"%(user.id,user.username,user.is_superuser),
+            user.is_superuser = not (user.is_superuser)
+            user.save()
+            print "->",
+            print "%d %s is_superuser=%s"%(user.id,user.username,user.is_superuser)
+        else:
+            print "user '%s' not found"%(username)
