4ad7b53e7af058d42571760d5be58ef817c3741e
chenwen9 <chenwen9@lenovo.com>
2015-11-18 11:19:15 +0800

    添加jqvalidate插件及showloadin蒙板插件

---
+++ shapps/ui/jquery/jqvalidate/template_plugins/jqvalidate.py
@@ -0,0 +1,4 @@
+def call(app, var, env):
+    a = []
+    a.append('validate/jquery.validate.min.js')
+    return {'toplinks': a}
---
+++ shapps/ui/jquery/showloading/template_plugins/showloading.py
@@ -0,0 +1,5 @@
+def call(app, var, env):
+    a = []
+    a.append('showloading/jquery.showLoading.js')
+    a.append('showloading/showLoading.css')
+    return {'toplinks': a}
